f0c5ab829b127bc20233f84dbea9f5d60679d326
pytbyte <heretolearn1@gmail.com>
2024-01-04 18:04:16 +0300

    Task 12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,7 @@
+export default function createReportObject(employeesList) {
+  const obj = {
+    allEmployees: employeesList,
+    getNumberOfDepartments() { return Object.keys(employeesList).length; },
+  };
+  return obj;
+}
