193b0518ce26101fe940d64ad0639b61b9c27d9c
pytbyte <heretolearn1@gmail.com>
2024-01-04 19:32:08 +0300

    Promise Task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+const handleResponseFromAPI = (promise) => promise
+  .then(() => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+  })
+  .catch(() => {
+    console.log('Got a response from the API');
+    throw new Error();
+  });
+
+export default handleResponseFromAPI;
