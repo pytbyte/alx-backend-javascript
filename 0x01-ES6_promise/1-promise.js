01e90d8df6a3bd6abc4f1f74624750c4764c2a6d
pytbyte <heretolearn1@gmail.com>
2024-01-04 19:29:07 +0300

    Ptomise Task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,5 @@
+const getFullResponseFromAPI = (success) => new Promise((resolve, reject) => (success
+  ? resolve({ status: 200, body: 'Success' })
+  : reject(new Error('The fake API is not working currently'))));
+
+export default getFullResponseFromAPI;
