1d338d9a70b20024797e1f444fd86e4951bf1ef9
pytbyte <heretolearn1@gmail.com>
2024-01-04 19:17:42 +0300

    Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,3 @@
-const getResponseFromAPI = () =>
-  new Promise((resolve) =>
-    setTimeout(() => resolve('API response data'), 1000)
-  );
-  
+const getResponseFromAPI = () => new Promise((resolve) => setTimeout(() => resolve('API response data'), 1000));
+
 export default getResponseFromAPI;
