e15779af27460a2840babf54005afbb98a827882
pytbyte <heretolearn1@gmail.com>
2024-01-04 19:37:38 +0300

    Ptomise Task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((responses) => {
+      const [photoResponse, userResponse] = responses;
+      console.log(
+        `${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`,
+      );
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
