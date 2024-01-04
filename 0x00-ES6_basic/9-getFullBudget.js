b045bd217c23a99c83a3f4c4e675d993edf72b37
pytbyte <heretolearn1@gmail.com>
2024-01-04 17:43:46 +0300

    Task 9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,9 +1,16 @@
-export default function getBudgetObject(income, gdp, capita) {
-  const budget = {
-    income,
-    gdp,
-    capita,
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
   };
 
-  return budget;
+  return fullBudget;
 }
