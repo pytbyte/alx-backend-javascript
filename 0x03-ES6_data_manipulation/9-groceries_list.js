fd371ca87698ae25a0990f86e2e16c940ed5218d
pytbyte <heretolearn1@gmail.com>
2024-01-14 11:29:40 +0300

    task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+/**
+ * Returns a map of groceries with items and quantities.
+ * @returns {Map} - The map of groceries.
+ */
+export default function groceriesList() {
+  const groceries = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+
+  return groceries;
+}
