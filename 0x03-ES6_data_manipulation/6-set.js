b626f95afe031a7d43da1ee24b5a8ff73f3b76f5
pytbyte <heretolearn1@gmail.com>
2024-01-14 11:17:19 +0300

    task 6

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * Creates a Set from an array.
+ * @param {Array} array - The array to convert to a Set.
+ * @returns {Set} - The Set created from the array.
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
