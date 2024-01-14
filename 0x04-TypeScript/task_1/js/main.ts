71c1f1abccabe6215391fb4a86ad5db3e0aadd33
pytbyte <heretolearn1@gmail.com>
2024-01-14 13:58:47 +0300

    typescript

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,51 @@
+export interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [index: string]: any;
+  }
+  
+  export interface Directors extends Teacher {
+    numberOfReports: number;
+  }
+  
+  export interface PrintTeacherFunction {
+    (firstName: string, lastName: string): string;
+  }
+  
+  export function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName[0]}. ${lastName}`;
+  }
+  
+  export interface StudentClassConstructor {
+    new (firstName: string, lastName: string): StudentClass;
+  }
+  
+  export interface StudentClass {
+    workOnHomework(): string;
+    displayName(): string;
+  }
+  
+  export class StudentClass implements StudentClass {
+    private _firstName!: string;
+    private _lastName!: string;
+  
+    constructor(firstName: string, lastName: string) {
+      this._firstName = firstName;
+      this._lastName = lastName;
+    }
+  
+    displayName(): string {
+      return this._firstName;
+    }
+  }
+  
+  export function createStudent(
+    ctor: StudentClassConstructor,
+    firstName: string,
+    lastName: string
+  ): StudentClass {
+    return new ctor(firstName, lastName);
+  }
