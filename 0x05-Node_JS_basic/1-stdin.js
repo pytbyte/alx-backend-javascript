26a9ae3ba4b02ab80068358c88f53ccfae0b206a
pytbyte <heretolearn1@gmail.com>
2024-03-03 09:33:11 +0300

    stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+/**
+ *reads frominput stream, printing the input 
+ *at the end of the program run.
+ */
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if ((chunk)) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
