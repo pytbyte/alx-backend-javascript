68afdf1e2912732932938cf5e4842e3f798eec54
pytbyte <heretolearn1@gmail.com>
2024-03-05 09:49:30 +0300

    unit test lifted

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,25 +1,20 @@
-import sendPaymentRequestToApi from './3-payment';
-import Utils from './utils';
-
 const sinon = require('sinon');
+const Utils = require('./utils');
 const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const consoleLogSpy = sinon.spy(console, 'log');
-    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
+    const bigBrother = sinon.spy(console);
+    const dummy = sinon.stub(Utils, 'calculateNumber');
 
-    calculateNumberStub.returns(10);
+    dummy.returns(10);
     sendPaymentRequestToApi(100, 20);
-
-    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
-    expect(calculateNumberStub.callCount).to.equal(1);
-
-    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
-    expect(consoleLogSpy.callCount).to.equal(1);
-
- 
-    calculateNumberStub.restore();
-    consoleLogSpy.restore();
+    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(dummy.callCount).to.be.equal(1);
+    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
+    expect(bigBrother.log.callCount).to.be.equal(1);
+    dummy.restore();
+    bigBrother.log.restore();
   });
 });
